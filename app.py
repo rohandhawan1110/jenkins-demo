ad12a88315cd352477abe86cf3b9ca3cb1378ad0
ROHAN DHAWAN <62983753+rohandhawan1110@users.noreply.github.com>
2026-09-17 03:46:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "Hello from Jenkins Docker build!"
+    return "Hello from Jenkins build 7!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
